62b6deec6c26d9fc0df73c374e91f6a39a9060a0
AFRA01 <AFRA01@30.152.57.234>
2018-05-15 12:07:57 -0700

    Initial

---
+++ src/main/java/com/antm/fdsm/caas/actadm/App.java
@@ -0,0 +1,13 @@
+package com.antm.fdsm.caas.actadm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
