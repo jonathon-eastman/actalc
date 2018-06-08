dba81112216a3806b0335e6b2dae5fb16084ada1
AFRA01 <AFRA01@30.152.58.146>
2018-06-08 15:19:10 -0700

    Add bso meta service + reporting fix.

--- src/main/java/com/antm/fdsm/caas/actadm/MetaAsoCubeService.java
+++ src/main/java/com/antm/fdsm/caas/actadm/MetaAsoCubeService.java
@@ -9,8 +9,8 @@ public class MetaAsoCubeService {
 	private EssbaseServer server = AnalyticCloudPlatform.getEssbaseServer("fdsm-dev-oac01.anthem.com");
 	private EssbaseCube metaAsoCube = server.getApplication(ServiceDefs.META_NAME_ASO_01).getCube(ServiceDefs.META_NAME_ASO_01);
 	
-	public RptgAsoCubeService prepareReportinCube() {
-		metaAsoCube.copyToNewApplication(ServiceDefs.RPTG_NAME_ASO_01).getCube(ServiceDefs.RPTG_NAME_ASO_01).rename(ServiceDefs.RPTG_NAME_ASO_01);
+	public RptgAsoCubeService prepareReportingCube() {
+		metaAsoCube.copyToNewApplication(ServiceDefs.RPTG_NAME_ASO_01).getCube(ServiceDefs.META_NAME_ASO_01).rename(ServiceDefs.RPTG_NAME_ASO_01);
 		RptgAsoCubeService cube = new RptgAsoCubeService();
 		return cube;
 	}
