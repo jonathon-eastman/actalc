c5da12753b46d39afbe18d25d8aa13821a40a071
AFRA01 <AFRA01@30.152.58.92>
2018-06-18 16:27:49 -0700

    Check to see if cube exists before deleting.

--- src/main/java/com/antm/fdsm/caas/actadm/MetaAsoCubeService.java
+++ src/main/java/com/antm/fdsm/caas/actadm/MetaAsoCubeService.java
@@ -1,6 +1,7 @@
 package com.antm.fdsm.caas.actadm;
 
 import com.antm.fdsm.orcl.oac.AnalyticCloudPlatform;
+import com.antm.fdsm.orcl.oac.EssbaseApplication;
 import com.antm.fdsm.orcl.oac.EssbaseCube;
 import com.antm.fdsm.orcl.oac.EssbaseServer;
 
@@ -10,6 +11,10 @@ public class MetaAsoCubeService {
 	private EssbaseCube metaAsoCube = server.getApplication(ServiceDefs.META_NAME_ASO_01).getCube(ServiceDefs.META_NAME_ASO_01);
 	
 	public RptgAsoCubeService prepareReportingCube() {
+		EssbaseApplication rptgAsoApp = server.getApplication(ServiceDefs.RPTG_NAME_ASO_01);
+		if (rptgAsoApp.exists()) {
+			rptgAsoApp.delete();
+		}
 		metaAsoCube.copyToNewApplication(ServiceDefs.RPTG_NAME_ASO_01).getCube(ServiceDefs.META_NAME_ASO_01).rename(ServiceDefs.RPTG_NAME_ASO_01);
 		RptgAsoCubeService cube = new RptgAsoCubeService();
 		return cube;
