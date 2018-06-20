ebefbe138fe2992548210e262b979594b2fbbcbb
AFRA01 <AFRA01@30.152.58.92>
2018-06-20 11:49:11 -0700

    Add export method.

--- src/main/java/com/antm/fdsm/caas/actadm/EssbaseCalculationService.java
+++ src/main/java/com/antm/fdsm/caas/actadm/EssbaseCalculationService.java
@@ -1,5 +1,15 @@
 package com.antm.fdsm.caas.actadm;
 
-public class EssbaseCalculationService {
+import com.antm.fdsm.orcl.oac.AnalyticCloudPlatform;
+import com.antm.fdsm.orcl.oac.EssbaseCube;
+import com.antm.fdsm.orcl.oac.EssbaseServer;
 
+public class EssbaseCalculationService {
+	
+	private EssbaseServer server = AnalyticCloudPlatform.getEssbaseServer("fdsm-dev-oac01.anthem.com");
+	private EssbaseCube calcBso01Cube = server.getApplication(ServiceDefs.CALC_NAME_BSO_01).getCube(ServiceDefs.CALC_NAME_BSO_01);
+	
+	public void exportCube( final String exportType) throws Exception {
+		calcBso01Cube.export(f -> f.fileName(ServiceDefs.RPTG_NAME_PRIMARY + "_" + exportType + ".txt"));
+	}
 }
