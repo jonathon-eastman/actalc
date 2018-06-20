3e936c90bc471cd00436321420c79e11d63077bb
AFRA01 <AFRA01@30.152.58.92>
2018-06-20 15:15:06 -0700

    add current period directory.

--- src/main/java/com/antm/fdsm/caas/actadm/RelationalDatabaseService.java
+++ src/main/java/com/antm/fdsm/caas/actadm/RelationalDatabaseService.java
@@ -12,8 +12,8 @@ public class RelationalDatabaseService {
 		hypusr.execute("truncate.sql");
 		hypusr.update("insert_from_gl.sql", new JsonArray().add(ServiceDefs.CURRENT_PERIOD).add(ServiceDefs.CURRENT_YEAR));
 		hypusr.update("update_project.sql");
-		hypusr.queryBlocking("query_monetary.sql", ServiceDefs.DIRECTORY_PROJECT + "/data/" + ServiceDefs.DIRECTORY_RELATIONAL + "/act_monetary_ap" + ServiceDefs.CURRENT_PERIOD + "_" + ServiceDefs.CURRENT_YEAR + ".txt");
-		hypusr.queryBlocking("query_stat.sql", ServiceDefs.DIRECTORY_PROJECT + "/data/" + ServiceDefs.DIRECTORY_RELATIONAL + "/act_statistical_ap" + ServiceDefs.CURRENT_PERIOD + "_" + ServiceDefs.CURRENT_YEAR + ".txt");
+		hypusr.queryBlocking("query_monetary.sql", ServiceDefs.DIRECTORY_PROJECT + "/data/" + ServiceDefs.DIRECTORY_CURRENTPERIOD + "/" + ServiceDefs.DIRECTORY_RELATIONAL + "/act_monetary_ap" + ServiceDefs.CURRENT_PERIOD + "_" + ServiceDefs.CURRENT_YEAR + ".txt");
+		hypusr.queryBlocking("query_stat.sql", ServiceDefs.DIRECTORY_PROJECT + "/data/" + ServiceDefs.DIRECTORY_CURRENTPERIOD + "/" + ServiceDefs.DIRECTORY_RELATIONAL + "/act_statistical_ap" + ServiceDefs.CURRENT_PERIOD + "_" + ServiceDefs.CURRENT_YEAR + ".txt");
 		hypusr.queryBlocking("balance_adm.sql", ServiceDefs.DIRECTORY_PROJECT + "/balances/act_adm" + ServiceDefs.CURRENT_PERIOD + "_" + ServiceDefs.CURRENT_YEAR + ".json");
 		hypusr.queryBlocking("balance_fte.sql", ServiceDefs.DIRECTORY_PROJECT + "/balances/act_fte" + ServiceDefs.CURRENT_PERIOD + "_" + ServiceDefs.CURRENT_YEAR + ".json");
 		hypusr.queryBlocking("balance_hct.sql", ServiceDefs.DIRECTORY_PROJECT + "/balances/act_hct" + ServiceDefs.CURRENT_PERIOD + "_" + ServiceDefs.CURRENT_YEAR + ".json");
