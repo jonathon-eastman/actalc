ee6a86e36b0cb1f42bcd4a675a8475735a8e43ea
AFRA01 <AFRA01@30.152.58.248>
2018-07-19 14:07:53 -0700

    get latest oac 0.5.3.

--- src/main/java/com/antm/fdsm/caas/actadm/Start.java
+++ src/main/java/com/antm/fdsm/caas/actadm/Start.java
@@ -1,12 +1,11 @@
 package com.antm.fdsm.caas.actadm;
 import com.antm.fdsm.orcl.utils.Singleton;
-
 import io.vertx.core.AbstractVerticle;
 
 public class Start extends AbstractVerticle {
 
 	private final static Singleton oacActService = Singleton.OACDEV.setDirs(Def.DIRS).setSlackApp(Def.SLACK_WEBHOOK_APP).setApp(Def.CUBE_NAME);
-	private final static Singleton dbHypusrService = Singleton.HYPUSR.setDirs(Def.DIRS).setSlackApp(Def.SLACK_WEBHOOK_APP).setApp(Def.CUBE_NAME);
+	private final static Singleton dbHypusrService = Singleton.HYPUSR.setDirs(Def.DIRS).setSlackApp(Def.SLACK_WEBHOOK_APP);
 
 	public static void main(String[] args) {
 		try {
@@ -14,9 +13,7 @@ public class Start extends AbstractVerticle {
 			//add security.
 			//test on server.
 			//localhost on mac + SSL.
-			//error handling.
 			//maybe change from runnable to observable
-			//date and move error files to more permanent location.  DO FIRST.
 			ServiceFacade.base(oacActService,dbHypusrService);
 			System.exit(0);
 		} catch (Exception e) {
