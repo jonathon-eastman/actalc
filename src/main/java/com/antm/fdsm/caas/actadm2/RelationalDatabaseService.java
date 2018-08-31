487325cf435663b2a83b842918d864c8731e5c34
AFRA01 <AFRA01@lc02tw8tvhtd5.us.ad.wellpoint.com>
2018-08-31 14:05:31 -0700

    test completeable code.

--- src/main/java/com/antm/fdsm/caas/actadm2/RelationalDatabaseService.java
+++ src/main/java/com/antm/fdsm/caas/actadm2/RelationalDatabaseService.java
@@ -4,6 +4,7 @@ package com.antm.fdsm.caas.actadm2;
 import com.antm.fdsm.orcl.odc.DatabaseService;
 import com.antm.fdsm.orcl.utils.Singleton;
 
+import io.reactivex.Completable;
 import io.vertx.core.json.JsonArray;
 
 public class RelationalDatabaseService {
@@ -23,9 +24,12 @@ public class RelationalDatabaseService {
 			.query2File("balance_adm.sql", Def.DIR_BALANCE + "/act_adm" + Def.CP + "_" + Def.CY + ".json")
 			.query2File("balance_fte.sql", Def.DIR_BALANCE + "/act_fte" + Def.CP + "_" + Def.CY + ".json")
 			.query2File("balance_hct.sql", Def.DIR_BALANCE + "/act_hct" + Def.CP + "_" + Def.CY + ".json")
-			.query2File("balance_hrs.sql", Def.DIR_BALANCE + "/act_hrs" + Def.CP + "_" + Def.CY + ".json")
-			.query2FileTraditional("query_act_monetary.sql",  Def.DIR_RELATIONAL + "/act_monetary_ap" + Def.CP + "_" + Def.CY + ".txt")
-			.query2FileTraditional("query_act_statistical.sql",  Def.DIR_RELATIONAL + "/act_statistical_ap" + Def.CP + "_" + Def.CY + ".txt");
+			.query2File("balance_hrs.sql", Def.DIR_BALANCE + "/act_hrs" + Def.CP + "_" + Def.CY + ".json");
+			
+			Completable c1 = hypusr.query2FileTraditional("query_act_monetary.sql",  Def.DIR_RELATIONAL + "/act_monetary_ap" + Def.CP + "_" + Def.CY + ".txt");
+			Completable c2 = hypusr.query2FileTraditional("query_act_statistical.sql",  Def.DIR_RELATIONAL + "/act_statistical_ap" + Def.CP + "_" + Def.CY + ".txt");
+			c1.blockingGet();
+			c2.blockingGet();
 			//.queryParallel(
 			//	Arrays.asList("query_act_monetary.sql","query_act_statistical.sql"),//, "balance_adm.sql","balance_fte.sql", "balance_hct.sql", "balance_hrs.sql"),
 			//	Arrays.asList(outputFile,outputFile.replace("monetary", "statistical"))//, Def.DIR_BALANCE + "/act_adm" + Def.CP + "_" + Def.CY + ".json", Def.DIR_BALANCE + "/act_fte" + Def.CP + "_" + Def.CY + ".json", Def.DIR_BALANCE + "/act_hct" + Def.CP + "_" + Def.CY + ".json", Def.DIR_BALANCE + "/act_hrs" + Def.CP + "_" + Def.CY + ".json")
