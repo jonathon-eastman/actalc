7d9ba993c5c4460a22f55c13ef9c9187a76fe0fb
AFRA01 <AFRA01@lc02tw8tvhtd5.us.ad.wellpoint.com>
2018-09-04 14:09:59 -0700

    update for new odc code.  will change everything to observables.

--- src/main/java/com/antm/fdsm/caas/actadm2/RelationalDatabaseService.java
+++ src/main/java/com/antm/fdsm/caas/actadm2/RelationalDatabaseService.java
@@ -23,12 +23,12 @@ public class RelationalDatabaseService {
 		hypusr.executeUpdate("insert_from_gl.sql", new JsonArray().add(Def.CP).add(Def.CY));
 		hypusr.executeUpdate("update_project.sql").blockingSingle();
 		
-		Observable<DatabaseService> qryAdm = hypusr.queryToFile("balance_adm.sql", Def.DIR_BALANCE + "/act_adm" + Def.CP + "_" + Def.CY + ".json");
-		Observable<DatabaseService> qryFte = hypusr.queryToFile("balance_fte.sql", Def.DIR_BALANCE + "/act_fte" + Def.CP + "_" + Def.CY + ".json");
-		Observable<DatabaseService> qryHct = hypusr.queryToFile("balance_hct.sql", Def.DIR_BALANCE + "/act_hct" + Def.CP + "_" + Def.CY + ".json");
-		Observable<DatabaseService> qryHrs = hypusr.queryToFile("balance_hrs.sql", Def.DIR_BALANCE + "/act_hrs" + Def.CP + "_" + Def.CY + ".json");
-		Observable<DatabaseService> qryMon = hypusr.queryToFile("query_act_monetary.sql",  Def.DIR_RELATIONAL + "/act_monetary_ap" + Def.CP + "_" + Def.CY + ".txt");
-		Observable<DatabaseService> qryStat = hypusr.queryToFile("query_act_statistical.sql",  Def.DIR_RELATIONAL + "/act_statistical_ap" + Def.CP + "_" + Def.CY + ".txt");
+		Observable<DatabaseService> qryAdm = hypusr.query2File("balance_adm.sql", Def.DIR_BALANCE + "/act_adm" + Def.CP + "_" + Def.CY + ".json");
+		Observable<DatabaseService> qryFte = hypusr.query2File("balance_fte.sql", Def.DIR_BALANCE + "/act_fte" + Def.CP + "_" + Def.CY + ".json");
+		Observable<DatabaseService> qryHct = hypusr.query2File("balance_hct.sql", Def.DIR_BALANCE + "/act_hct" + Def.CP + "_" + Def.CY + ".json");
+		Observable<DatabaseService> qryHrs = hypusr.query2File("balance_hrs.sql", Def.DIR_BALANCE + "/act_hrs" + Def.CP + "_" + Def.CY + ".json");
+		Observable<DatabaseService> qryMon = hypusr.query2File("query_act_monetary.sql",  Def.DIR_RELATIONAL + "/act_monetary_ap" + Def.CP + "_" + Def.CY + ".txt");
+		Observable<DatabaseService> qryStat = hypusr.query2File("query_act_statistical.sql",  Def.DIR_RELATIONAL + "/act_statistical_ap" + Def.CP + "_" + Def.CY + ".txt");
 		qryAdm.blockingSubscribe( db -> Logger.info("finished adm balance query."));
 		qryFte.blockingSubscribe( db -> Logger.info("finished fte balance query."));
 		qryHct.blockingSubscribe( db -> Logger.info("finished hct balance query."));
