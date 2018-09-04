d35740d34822eec6f7ad3aefcd24514353740f02
AFRA01 <AFRA01@lc02tw8tvhtd5.us.ad.wellpoint.com>
2018-09-04 13:51:56 -0700

    update for more parallel code.

--- src/main/java/com/antm/fdsm/caas/actadm2/RelationalDatabaseService.java
+++ src/main/java/com/antm/fdsm/caas/actadm2/RelationalDatabaseService.java
@@ -1,12 +1,13 @@
 package com.antm.fdsm.caas.actadm2;
 
 
+import org.pmw.tinylog.Logger;
+
 import com.antm.fdsm.orcl.odc.DatabaseService;
 import com.antm.fdsm.orcl.utils.Singleton;
-
-import io.reactivex.Completable;
 import io.reactivex.Observable;
 import io.vertx.core.json.JsonArray;
+
 
 public class RelationalDatabaseService {
 	private Singleton dbService;
@@ -18,23 +19,23 @@ public class RelationalDatabaseService {
 	public void extractPSGLCurrentMonth() {
 
 		DatabaseService hypusr = new DatabaseService(dbService);
-		String outputFile = Def.DIR_RELATIONAL + "/act_monetary_ap" + Def.CP + "_" + Def.CY + ".txt";
-		hypusr.execute("truncate.sql")
-			.querySingleWithParams("insert_from_gl.sql", new JsonArray().add(Def.CP).add(Def.CY))
-			.update("update_project.sql")
-			.query2File("balance_adm.sql", Def.DIR_BALANCE + "/act_adm" + Def.CP + "_" + Def.CY + ".json")
-			.query2File("balance_fte.sql", Def.DIR_BALANCE + "/act_fte" + Def.CP + "_" + Def.CY + ".json")
-			.query2File("balance_hct.sql", Def.DIR_BALANCE + "/act_hct" + Def.CP + "_" + Def.CY + ".json")
-			.query2File("balance_hrs.sql", Def.DIR_BALANCE + "/act_hrs" + Def.CP + "_" + Def.CY + ".json");
-			
-			Observable<DatabaseService> x1 = hypusr.query2FileTraditional("query_act_monetary.sql",  Def.DIR_RELATIONAL + "/act_monetary_ap" + Def.CP + "_" + Def.CY + ".txt");
-			Observable<DatabaseService> x2 = hypusr.query2FileTraditional("query_act_statistical.sql",  Def.DIR_RELATIONAL + "/act_statistical_ap" + Def.CP + "_" + Def.CY + ".txt");
-			x1.blockingSubscribe( db -> System.out.println("mon done!"));
-			x2.blockingSubscribe( db -> System.out.println("stat done!"));
-			//.queryParallel(
-			//	Arrays.asList("query_act_monetary.sql","query_act_statistical.sql"),//, "balance_adm.sql","balance_fte.sql", "balance_hct.sql", "balance_hrs.sql"),
-			//	Arrays.asList(outputFile,outputFile.replace("monetary", "statistical"))//, Def.DIR_BALANCE + "/act_adm" + Def.CP + "_" + Def.CY + ".json", Def.DIR_BALANCE + "/act_fte" + Def.CP + "_" + Def.CY + ".json", Def.DIR_BALANCE + "/act_hct" + Def.CP + "_" + Def.CY + ".json", Def.DIR_BALANCE + "/act_hrs" + Def.CP + "_" + Def.CY + ".json")
-		//	);
+		hypusr.executeUpdate("truncate.sql").blockingSingle();
+		hypusr.executeUpdate("insert_from_gl.sql", new JsonArray().add(Def.CP).add(Def.CY));
+		hypusr.executeUpdate("update_project.sql").blockingSingle();
+		
+		Observable<DatabaseService> qryAdm = hypusr.queryToFile("balance_adm.sql", Def.DIR_BALANCE + "/act_adm" + Def.CP + "_" + Def.CY + ".json");
+		Observable<DatabaseService> qryFte = hypusr.queryToFile("balance_fte.sql", Def.DIR_BALANCE + "/act_fte" + Def.CP + "_" + Def.CY + ".json");
+		Observable<DatabaseService> qryHct = hypusr.queryToFile("balance_hct.sql", Def.DIR_BALANCE + "/act_hct" + Def.CP + "_" + Def.CY + ".json");
+		Observable<DatabaseService> qryHrs = hypusr.queryToFile("balance_hrs.sql", Def.DIR_BALANCE + "/act_hrs" + Def.CP + "_" + Def.CY + ".json");
+		Observable<DatabaseService> qryMon = hypusr.queryToFile("query_act_monetary.sql",  Def.DIR_RELATIONAL + "/act_monetary_ap" + Def.CP + "_" + Def.CY + ".txt");
+		Observable<DatabaseService> qryStat = hypusr.queryToFile("query_act_statistical.sql",  Def.DIR_RELATIONAL + "/act_statistical_ap" + Def.CP + "_" + Def.CY + ".txt");
+		qryAdm.blockingSubscribe( db -> Logger.info("finished adm balance query."));
+		qryFte.blockingSubscribe( db -> Logger.info("finished fte balance query."));
+		qryHct.blockingSubscribe( db -> Logger.info("finished hct balance query."));
+		qryHrs.blockingSubscribe( db -> Logger.info("finished hrs balance query."));
+		qryMon.blockingSubscribe( db -> Logger.info("finished monetary amount extract query."));
+		qryStat.blockingSubscribe( db -> System.out.println("finished statistical amount extract query."));
+
 	}
 
 }
