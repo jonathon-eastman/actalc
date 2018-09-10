f4e3d7ba90f84f6c4e5c985726db809fa7f63f56
AFRA01 <AFRA01@lc02tw8tvhtd5.us.ad.wellpoint.com>
2018-09-10 10:51:57 -0700

    test.

--- src/main/java/com/antm/fdsm/caas/actadm2/ServiceFacade.java
+++ src/main/java/com/antm/fdsm/caas/actadm2/ServiceFacade.java
@@ -17,12 +17,12 @@ public class ServiceFacade {
 		RelationalDatabaseService relationalService = new RelationalDatabaseService(dbService);
 
 		final EssbaseMetadataService metaService = new EssbaseMetadataService(oacService);
-		CompletableFuture<Void> createRptg =  metaService.createReportingCube();
+		//CompletableFuture<Void> createRptg =  metaService.createReportingCube();
 		CompletableFuture<Void> createCalc =  metaService.createCalculatingCube();
-		CompletableFuture<Void> extract = relationalService.extractPSGLCurrentMonth();
+		//CompletableFuture<Void> extract = relationalService.extractPSGLCurrentMonth();
 
 		
-		extract.get();
+		//extract.get();
 		createCalc.get();
 		
 		Logger.info("calc cube creation completed.");
