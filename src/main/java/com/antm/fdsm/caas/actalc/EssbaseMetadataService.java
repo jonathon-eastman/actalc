f960b28edac5be074dd9f28bde1e499e1ee26760
AFRA01 <AFRA01@lc02tw8tvhtd5.us.ad.wellpoint.com>
2018-11-06 10:05:44 -0800

    test 9.

--- src/main/java/com/antm/fdsm/caas/actalc/EssbaseMetadataService.java
+++ src/main/java/com/antm/fdsm/caas/actalc/EssbaseMetadataService.java
@@ -40,7 +40,7 @@ public class EssbaseMetadataService {
 				metaOtl.deleteMember("Alt Company Hierarchies");
 				metaOtl.deleteMember("Unconsolidated Companies");
 				metaOtl.deleteMember("Company Alloc");
-				metaOtl.deleteDimension("Time Periods");
+				//metaOtl.deleteDimension("Time Periods");
 				/*metaOtl.addDimension( dim -> dim
 					.name("Time Periods blah")
 					.previousSibling("Funding Type")
@@ -51,14 +51,14 @@ public class EssbaseMetadataService {
 
 				metaOtl.deleteMember("Alt Product Structures");
 				metaOtl.deleteMember("Product Alloc");
-				metaOtl.deleteMember("DOI Product");
-				metaOtl.deleteMember("MBU Alloc");
-				metaOtl.deleteMember("Drivers");
-				metaOtl.deleteMember("Scenarios");
-				metaOtl.deleteMember("CBE");
-				metaOtl.deleteMember("Brand State");
-				metaOtl.deleteMember("Product Type");
-				metaOtl.deleteMember("CC Function");
+				//metaOtl.deleteMember("DOI Product");
+				//metaOtl.deleteMember("MBU Alloc");
+				//metaOtl.deleteMember("Drivers");
+				//metaOtl.deleteMember("Scenarios");
+				//metaOtl.deleteMember("CBE");
+				//metaOtl.deleteMember("Brand State");
+				//metaOtl.deleteMember("Product Type");
+				//metaOtl.deleteMember("CC Function");
 				/*metaOtl.addMember(mbr -> mbr
 					.name("BegBalance")
 					.parent("Time Periods")
