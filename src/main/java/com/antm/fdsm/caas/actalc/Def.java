01c91a4f1ddc0acbde380a0e9eb77fe742373491
AFRA01 <AFRA01@192.168.55.243>
2018-11-05 16:36:53 -0800

    update month and source file for drivers.

--- src/main/java/com/antm/fdsm/caas/actalc/Def.java
+++ src/main/java/com/antm/fdsm/caas/actalc/Def.java
@@ -38,7 +38,7 @@ public class Def {
 	public static final List<String> DIRS = Arrays.asList(DIR_PROJECT,DIR_BALANCE,DIR_BKP,DIR_TMP,DIR_DATA,DIR_HISTORY,DIR_CP,DIR_EXPORT,DIR_RELATIONAL,DIR_CPHISTORY,DIR_NEW,DIR_PREVIOUS,DIR_INCREMENTAL, DIR_LAST, DIR_LOG, DIR_MDX, DIR_REQUIRED, DIR_DRIVERS);
 	public static final Map<String, String> LINKS = new HashMap<String, String>();
 	
-	public static final int CP  = 9;
+	public static final int CP  = 10;
 	public static final int CY  = 2018;
 	public static final int YR2D = 18;
 	public static final int YR2DS1 = 17;
@@ -50,7 +50,7 @@ public class Def {
 		LINKS.put("/wh/h_" + CUBE_NAME_SOURCE.toLowerCase() + "_af48_" + YR2D + ".txt", DIR_HISTORY);
 		LINKS.put("/wh/h_" + CUBE_NAME_SOURCE.toLowerCase() + "_wp_" + YR2D + ".txt", DIR_HISTORY);
 		LINKS.put("/wh/h_" + CUBE_NAME_SOURCE.toLowerCase() + "_ar_" + YR2D + ".txt", DIR_CPHISTORY);
-		LINKS.put("/tstalc/data/export/required/tstalc_r1.txt", DIR_DRIVERS);
+		LINKS.put("/js/tstalc/data/export/required/" + CUBE_NAME_SOURCE.toLowerCase() + ".txt", DIR_DRIVERS);
 	};
 
 	public static final int VARIANCE_TOLERANCE = 1;
