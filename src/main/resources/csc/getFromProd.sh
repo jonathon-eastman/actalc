3c44968f684b0cb2df0b292fd350b7859122939b
AFRA01 <AFRA01@30.152.56.219>
2018-11-29 16:17:07 -0800

    automatically update subvars.

--- src/main/resources/csc/getFromProd.sh
+++ src/main/resources/csc/getFromProd.sh
@@ -8,3 +8,12 @@ mv par_dtl6.csc allocate_region6.csc
 rm prg_E*.csc
 rm par_ClrA.csc
 rm par_qi.csc
+# Change sub var in csc files
+csc_files=`ls ./*.csc`
+csc_files=( $csc_files )
+for csc_file in ${csc_files[*]}
+do
+  mod_csc_file=${csc_file}.mod
+  perl -p -e 's/&cur_per_a/<<CURRENT_PERIOD_ACTUAL>>/gi' <$csc_file > $mod_csc_file
+  mv $mod_csc_file $csc_file
+done
