0135eeb0bd58df5ffcc12592abdde55e87cda923
AG16386 <AG16386@lfc8x3m2.us.ad.wellpoint.com>
2019-07-03 14:30:41 -0400

    Reverting fix statement back

--- src/main/java/com/antm/fdsm/caas/actalc/EssbaseCalculationService.java
+++ src/main/java/com/antm/fdsm/caas/actalc/EssbaseCalculationService.java
@@ -65,7 +65,7 @@ public class EssbaseCalculationService {
 	}
 	
 	private static CompletableFuture<AnalyticExportFile> exportWithFixStatement(EssbaseCube cube,String strHome, String str ) {
-		String fix = "FIX (@RELATIVE(\"Company\", 0), @RELATIVE(\"Funding Type Total\", 0),@RELATIVE(\"Fixed Pool Total\", 0),@RELATIVE(\"" + str +"\", 0),@RELATIVE(\"Product Total\", 0),@RELATIVE(\"" + str + "\", 0), \"Admin Exp Alloc\", \"" + str + "\", Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec)"; 
+		String fix = "FIX (@RELATIVE(\"Company\", 0), @RELATIVE(\"Funding Type Total\", 0),@RELATIVE(\"Fixed Pool Total\", 0),@RELATIVE(\"" + str +"\", 0),@RELATIVE(\"Product Total\", 0),@RELATIVE(\"" + str + "\", 0), \"Admin Exp Alloc\", \"" + str + "\", " + Helpers.translateMonthNumber(Def.CP) + ")"; 
 		CompletableFuture<AnalyticExportFile> export = null;
 		try {
 			export = cube.export(f -> f
