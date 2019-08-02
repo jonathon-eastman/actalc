1b82705d3cd6dc1ec1b2059550e2f4ee2303643e
AG16386 <AG16386@lfc8x3m2.us.ad.wellpoint.com>
2019-08-02 10:52:31 -0400

    Testing DBG Export

--- src/main/java/com/antm/fdsm/caas/actalc/EssbaseCalculationService.java
+++ src/main/java/com/antm/fdsm/caas/actalc/EssbaseCalculationService.java
@@ -88,7 +88,7 @@ public class EssbaseCalculationService {
 	}
 	
 	private static CompletableFuture<AnalyticExportFile> exportDBGWithFixStatement(EssbaseCube cube,String strHome, String str ) {
-		String fix = "FIX (@RELATIVE(\"WellPoint, Inc. (Cons)\", 0), @RELATIVE(\"Funding Type Total\", 0),@RELATIVE(\"Fixed Pool Total\", 0), @RELATIVE(\"Product Total\", 0),@RELATIVE(\"Diversified Business Group\", 0), \"Admin Exp Alloc\", Helpers.translateMonthNumber(Def.CP) + )"; 
+		String fix = "FIX (@RELATIVE(\"WellPoint, Inc. (Cons)\", 0), @RELATIVE(\"Funding Type Total\", 0),@RELATIVE(\"Fixed Pool Total\", 0),@RELATIVE(\"" + str +"\", 0),@RELATIVE(\"Product Total\", 0),@RELATIVE(\"" + str +"\", 0),@RELATIVE(\"Diversified Business Group\", 0),@RELATIVE(\"" + str + "\", 0), \"Admin Exp Alloc\", \"" + str + "\", " + Helpers.translateMonthNumber(Def.CP) + ")"; 
 		CompletableFuture<AnalyticExportFile> export = null;
 		try {
 			export = cube.export(f -> f
