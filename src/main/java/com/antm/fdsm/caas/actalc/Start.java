dc2c78548485cf501198dd264ded4a7e710107ce
ag16386 <ag16386@LFC8X3M2.us.ad.wellpoint.com>
2020-01-07 15:41:01 -0500

    update for OCI2

--- src/main/java/com/antm/fdsm/caas/actalc/Start.java
+++ src/main/java/com/antm/fdsm/caas/actalc/Start.java
@@ -4,16 +4,16 @@ import org.pmw.tinylog.Logger;
 import com.antm.fdsm.orcl.odc.services.HYPUSR;
 import com.antm.fdsm.orcl.odc.services.OracleOptions;
 import com.antm.fdsm.orcl.odc.services.OracleService;
+import com.antm.fdsm.orcl.oac.services.ESSACT;
 import com.antm.fdsm.orcl.oac.services.EssbaseAnalyticsOptions;
 import com.antm.fdsm.orcl.oac.services.EssbaseAnalyticsService;
-import com.antm.fdsm.orcl.oac.services.OACACT;
 import com.antm.fdsm.orcl.utils.GlobalOptions;
 
 import io.vertx.core.AbstractVerticle;
 
 public class Start extends AbstractVerticle {
 
-	private final static EssbaseAnalyticsService essbase = OACACT.configure(new EssbaseAnalyticsOptions().setName(Def.PROJECT_NAME).setHome(GlobalOptions.HOME).setSlack(Def.SLACK_WEBHOOK_APP).setLinks(Def.LINKS));
+	private final static EssbaseAnalyticsService essbase = ESSACT.configure(new EssbaseAnalyticsOptions().setName(Def.PROJECT_NAME).setHome(GlobalOptions.HOME).setSlack(Def.SLACK_WEBHOOK_APP).setLinks(Def.LINKS));
 	private final static OracleService hypusr = HYPUSR.configure(new OracleOptions().setName(Def.PROJECT_NAME).setHome(GlobalOptions.HOME));
 
 	
